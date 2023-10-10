7e92b9d4701dd7709f64bf3bb256dae01ed262cd
Ahmed Karim <muhammadahmedkarimnagda@gmail.com>
2023-10-11 01:41:41 +0500

    make some UI

---
+++ app.js
@@ -0,0 +1,13 @@
+var gharriStore = {
+    band : {
+        tomi : {
+            TomiFaceGear:{
+                name : "TOMI FaceGear",
+                description : "Original Tomi FaceGear Series",
+                price : "Rs.3,500.00 PKR",
+                frontImg : "./img/img-1-front.webp",
+                backImg : "./img/img-1-back.webp"
+            }
+        }
+    }
+}
